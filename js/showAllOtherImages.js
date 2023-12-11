11894d0d1884fb6457806328dd4562091d28049f
Nikola Bukvic <117097070+nikola017@users.noreply.github.com>
2023-12-11 05:33:33 +0100

    lab4 commit

---
+++ js/showAllOtherImages.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const allImagesSection = document.querySelector('#imageGallery');
+    const otherImages = ['bird.jpg', 'bird.jpg', 'BlueRook.png', 'coding-logo.png', 'dragon.jpg', 'kratos.jpg', 'low_res_bird.png', 'silence.jpg', 'sillyFace.png', 'soccer.png', 'them_pixels.jpg'];
+
+    otherImages.forEach(image => {
+        const img = document.createElement('img');
+        img.src = `images/${image}`;
+        img.alt = image;
+        allImagesSection.appendChild(img);
+    });
+});
